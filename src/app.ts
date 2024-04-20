a392f41ad0c02ce0fd52ed4964b7acd43c6dd108
ranjeet-programmer <ranjeethingeofficial@gmail.com>
2024-04-20 15:38:27 +0530

    error handling added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
-import express from 'express';
-
+import express, { NextFunction, Request, Response } from 'express';
+import logger from './config/logger';
+import { HttpError } from 'http-errors';
 const app = express();
 
-app.get('/', (req: any, res: any) => {
+app.get('/', async (req: any, res: any) => {
     res.send('welcome to auth service');
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.status || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                message: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    });
 });
 
 export default app;
